1a1502a09501b26e39a8c72e13ffa808d4179177
EthanMcNalley <ethanmcnalley13@gmail.com>
2025-07-10 15:10:56 -0400

    wooooooOoooo

---
+++ Assets/Scripts/Avatar/AvatarAI.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AvatarAI : MonoBehaviour
+{
+    NavMeshAgent agent;
+    [SerializeField] LayerMask ground_layer;
+
+    //Patrolling Variables
+    Vector3 destination_point;
+    bool walking_point_set;
+    [SerializeField] float range;
+    float roaming_count = 0.0f;
+    float not_roaming_count = 0.0f;
+    float roam_timer = 0.0f;
+    public float min_roam_time = 5.0f;
+    public float max_roam_time = 15.0f;
+    public float min_stand_time = 5.0f;
+    public float max_stand_time = 15.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        roaming_count = Random.Range(min_roam_time, max_roam_time);
+        not_roaming_count = Random.Range(min_stand_time, max_stand_time);
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Patrol();
+        roam_timer += Time.deltaTime;
+        Debug.Log(roam_timer);
+    }
+
+    void Patrol(){
+        
+        if (roam_timer < roaming_count){
+            if (!walking_point_set){
+            SearchForPoint();
+            }
+
+            if (walking_point_set){
+                agent.SetDestination(destination_point);
+            }
+
+            if (Vector3.Distance(transform.position, destination_point) < 10) walking_point_set = false;
+
+        }
+
+        else{
+            if (roam_timer > (roaming_count + not_roaming_count)){
+                roam_timer = 0.0f;
+                roaming_count = Random.Range(min_roam_time, max_roam_time);
+                not_roaming_count = Random.Range(min_stand_time, max_stand_time);
+            }
+        }
+        
+    }
+
+    void SearchForPoint(){
+        float z_pos = Random.Range(-range, range);
+        float x_pos = Random.Range(-range, range);
+
+        destination_point = new Vector3(transform.position.x + x_pos, transform.position.y, transform.position.z + z_pos);
+
+        if(Physics.Raycast(destination_point, Vector3.down, ground_layer)){
+            walking_point_set = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Avatar/HttpClient.cs
@@ -0,0 +1,39 @@
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+
+public class HttpClient
+{
+    private readonly IserializationOption _serialization_option;
+    public HttpClient(IserializationOption serialization_option){
+        _serialization_option = serialization_option;
+    }
+
+    public async Task<TResultType> Get<TResultType>(string url){
+        try{
+
+            using var www = UnityWebRequest.Get(url);
+
+            www.SetRequestHeader("Content-Type", _serialization_option.content_type);
+
+            var operation = www.SendWebRequest();
+
+            while(!operation.isDone){
+                await Task.Yield();
+            }
+
+            if (www.result != UnityWebRequest.Result.Success){
+                Debug.LogError($"Failed: {www.error}");
+            }
+                var result = _serialization_option.Deserialize<TResultType>(www.downloadHandler.text);
+                Debug.Log($"Success: {www.downloadHandler.text}");
+                return result;
+        }
+
+        catch(Exception ex){
+            Debug.LogError($"{nameof(Get)} failed: {ex.Message}");
+            return default;
+        }
+    }
+}
