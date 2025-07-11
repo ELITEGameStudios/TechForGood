c7865446f0e9ae5febd1c6761c2670fba8149b97
andrewh <mrrobottrax@gmail.com>
2025-07-11 11:59:19 -0400

    Add clothing tabs

--- .web-server/YouNiverse/Models/Youniverse/CosmeticItem.cs
+++ .web-server/YouNiverse/Models/Youniverse/CosmeticItem.cs
@@ -1,13 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
 namespace YouNiverse.Models.Youniverse;
 
 public enum EItemSlot
 {
+	[Display(Name = "Hats")]
 	Hat,
+	[Display(Name = "Pets")]
 	Pet,
+	[Display(Name = "Shoes")]
 	Shoes,
+	[Display(Name = "Shirts")]
 	Shirt,
+	[Display(Name = "Pants")]
 	Pants,
 }
+
+public static class EnumHelpers
+{
+	public static string GetDisplayName(Enum value)
+	{
+		var field = value.GetType().GetField(value.ToString());
+		var attr = field?.GetCustomAttribute<DisplayAttribute>();
+		return attr?.Name ?? value.ToString();
+	}
+}
+
 
 public class CosmeticItem
 {
