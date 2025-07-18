a69d5c5acac96c7445bc7a8d77ed9927f784a692
EthanMcNalley <ethanmcnalley13@gmail.com>
2025-07-18 12:57:11 -0400

    yus

--- Assets/Scripts/Time/RealTime.cs
+++ Assets/Scripts/Time/RealTime.cs
@@ -16,7 +16,7 @@ public class RealTime : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        minute_hand.transform.eulerAngles = new Vector3(0,90,0);
+        minute_hand.transform.eulerAngles = new Vector3(0, 90, 0);
         time = System.DateTime.UtcNow.ToLocalTime();
         Debug.Log(time);
     }
